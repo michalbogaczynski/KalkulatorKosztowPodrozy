9fdea11e8cc95b62605a05752489fe52c84bb3d7
michalbogaczynski <bogaczynski.m@gmail.com>
2018-11-27 23:01:12 +0100

    Połaczone opereatory.

--- KalkulatorKosztowPodrozy/Form1.cs
+++ KalkulatorKosztowPodrozy/Form1.cs
@@ -12,9 +12,42 @@ namespace KalkulatorKosztowPodrozy
 {
     public partial class Form1 : Form
     {
+
+        int startingMileage;
+        int endingMileage;
+        double milesTraveled;
+        double reimburseRate = .39;
+        double amountOwed;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            startingMileage = (int)numericUpDown1.Value;
+            endingMileage = (int)numericUpDown2.Value;
+
+            if (startingMileage <= endingMileage)
+            {
+                milesTraveled = endingMileage - startingMileage;
+                amountOwed = milesTraveled * reimburseRate;
+                label4.Text = amountOwed.ToString() + " zł";
+            }
+            else
+            {
+                MessageBox.Show("Początkowy stan licznika musi być mniejszy niż końcowy", "Nie mogę obliczyć odległości");
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(milesTraveled + " kilometrów", "Przebyta odległość");
         }
     }
 }
